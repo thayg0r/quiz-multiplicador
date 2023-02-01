97d7adefd25b60eca57e8b53aa1d5bd8b1a05ea7
thayg0r <thaygor@live.com>
2023-02-01 02:39:01 -0300

    quiz-multiplicador

---
+++ index.js
@@ -0,0 +1,37 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+const questionEl = document.getElementById ("question");
+
+const inputEl = document.getElementById("input");
+
+const formEl = document.getElementById("form");
+
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if(!score){
+    score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`
+
+questionEl.innerText = `Quanto é ${num1} multiplicado por ${num2}?`;
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+    const userAns = +inputEl.value;
+    if(userAns === correctAns) {
+        score++;
+        updatelocalStorage()
+    } else {
+        score--;
+        updatelocalStorage()
+    }
+})
+
+function updatelocalStorage(){
+    localStorage.setItem("score", JSON.stringify(score))
+}
